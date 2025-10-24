00e82d8bc519c3bf36cf3f1aec1d65df0358932a
Vaibhav5771 <vaibhavpardeshi190@gmail.com>
2025-10-24 07:03:55 +0530

    Implemented 3D ProductVector

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,26 +1,77 @@
-import React from 'react'
+import React from 'react';
+import useMacbookStore from "../store/index.js";
+import clsx from "clsx";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14.jsx";
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from "./three/ModelSwitcher.jsx";
+import { useMediaQuery } from "react-responsive";
 
 const ProductViewer = () => {
+    const { color, scale, setColor, setScale } = useMacbookStore();
+    const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+
     return (
         <section id="product-viewer">
             <h2>Take a closer look.</h2>
+
             <div className="controls">
-                <p className="info">MacBookPro 16" in Space Black</p>
+                {/*<p className="info">*/}
+                {/*    MacBook Pro | Available in 14" and 16" in Space Grey and Dark colors*/}
+                {/*</p>*/}
+
                 <div className="flex-center gap-5 mt-5">
+                    {/* Color Controls */}
                     <div className="color-control">
-                        <div className="bg-neutral-300"/>
-                        <div className="bg-neutral-900"/>
+                        <div
+                            onClick={() => setColor('#adb5bd')}
+                            className={clsx(
+                                'bg-neutral-300 w-6 h-6 rounded-full cursor-pointer',
+                                color === '#adb5bd' && 'active'
+                            )}
+                        />
+                        <div
+                            onClick={() => setColor('#2e2c2e')}
+                            className={clsx(
+                                'bg-neutral-900 w-6 h-6 rounded-full cursor-pointer',
+                                color === '#2e2c2e' && 'active'
+                            )}
+                        />
                     </div>
 
-                    <div className="size-control">
-                        <div><p>14"</p></div>
-                        <div><p>16"</p></div>
-                    </div>
+                    {/* Size Controls */}
+                    <div className="size-control flex gap-2">
+                        <div
+                            onClick={() => setScale(0.06)}
+                            className={clsx(
+                                scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white'
+                            )}
+                        >
+                            <p>14"</p>
+                        </div>
 
+                        <div
+                            onClick={() => setScale(0.08)}
+                            className={clsx(
+                                scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white'
+                            )}
+                        >
+                            <p>16"</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <p className="text-white text-4xl">Render Canvas</p>
+
+            <Canvas
+                id="canvas"
+                camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+            >
+                <StudioLights />
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
+            </Canvas>
         </section>
-    )
-}
-export default ProductViewer
+    );
+};
+
+export default ProductViewer;
